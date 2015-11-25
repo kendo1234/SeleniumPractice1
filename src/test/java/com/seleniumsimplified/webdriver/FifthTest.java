88a5012882fa3fb1bf5d240be49561f3d585908f
kendo1234 <kendomustdie@googlemail.com>
2015-11-25 21:11:37 +0000

    Commit

--- src/test/java/com/seleniumsimplified/webdriver/FifthTest.java
+++ src/test/java/com/seleniumsimplified/webdriver/FifthTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 public class FifthTest {
@@ -14,7 +15,7 @@ public class FifthTest {
 
         driver.get("http://selenium.googlecode.com/svn/trunk/docs/api/java/index.html");
         driver.switchTo().frame("classFrame"); //Switches to the frame that contains "Deprecated"
-        driver.findElement(By.linkText("Deprecated")).click(); //Clicks "Deprecated" link
+        driver.findElement(By.linkText("Deprecated")).click();
         driver.quit();
 
     }
